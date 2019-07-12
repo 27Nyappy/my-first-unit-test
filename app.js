27b3102bccc65a289b87a0182920bdb526f5902a
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-07-11 19:37:26 -0700

    addition

---
+++ app.js
@@ -0,0 +1,15 @@
+import add from './add.js';
+const xInput = document.getElementById('x');
+const yInput = document.getElementById('y');
+const sum = document.getElementById('sum');
+
+//wire up events
+xInput.oninput = showSum;
+yInput.oninput = showSum;
+
+function showSum() {
+    const x = Number(xInput.value);
+    const y = Number(yInput.value);
+
+    sum.textContext = add(x, y);
+}
